fa197340cb510ee5f08d3789b940f90bb99e70c4
NaCl Prep <admin@naclprep.com>
2025-08-11 05:20:01 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <a href="mailto:ad

--- src/components/landing/cta-section.tsx
+++ src/components/landing/cta-section.tsx
@@ -22,7 +22,7 @@ export default function CtaSection() {
           Join the growing number of hotels delighting their guests with Swift180. Get in touch with our team today to schedule a demo and learn how we can tailor a solution for you.
         </p>
         <div className="mt-8">
-          <Button size="lg" variant="secondary" asChild>
+          <Button size="lg" variant="outline" className="bg-transparent border-white text-white hover:bg-white/10 hover:text-white" asChild>
             <a href={mailtoLink}>Request a Demo</a>
           </Button>
         </div>
