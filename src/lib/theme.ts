7d2f53c556ace7d9dd0f0664bf05fe2d8d2870cb
NaCl Prep <admin@naclprep.com>
2025-08-11 02:03:20 +0000

    move all the color font and theme related things to a theme constant fil

---
+++ src/lib/theme.ts
@@ -0,0 +1,8 @@
+
+export const theme = {
+  fontFamily: {
+    body: ['Roboto', 'sans-serif'],
+    headline: ['Lato', 'sans-serif'],
+    code: ['monospace'],
+  },
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type {Config} from 'tailwindcss';
+import {theme} from './src/lib/theme';
 
 export default {
   darkMode: ['class'],
@@ -9,11 +10,7 @@ export default {
   ],
   theme: {
     extend: {
-      fontFamily: {
-        body: ['Roboto', 'sans-serif'],
-        headline: ['Lato', 'sans-serif'],
-        code: ['monospace'],
-      },
+      fontFamily: theme.fontFamily,
       colors: {
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
